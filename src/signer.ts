bd3c335aa03046d69debdda909ac1a6c6ecf4564
Sergey Bakulin <vansergen@gmail.com>
2019-11-21 17:03:44 -0500

    feat(module): add signer

---
+++ src/signer.ts
@@ -0,0 +1,44 @@
+import * as crypto from "crypto";
+import { stringify, ParsedUrlQuery } from "querystring";
+
+export type SignerOptions = {
+  method: string;
+  uri: string;
+  body?: object | "";
+  key: string;
+  secret: string;
+  passphrase: string;
+  timestamp?: number;
+  qs?: ParsedUrlQuery;
+};
+
+export type SignedHeaders = {
+  "CB-ACCESS-KEY": string;
+  "CB-ACCESS-SIGN": string;
+  "CB-ACCESS-TIMESTAMP": number;
+  "CB-ACCESS-PASSPHRASE": string;
+};
+
+export function Signer({
+  method,
+  uri,
+  body = {},
+  key,
+  secret,
+  passphrase,
+  timestamp = Date.now() / 1000,
+  qs
+}: SignerOptions): SignedHeaders {
+  if (qs && Object.keys(qs).length) {
+    uri += "?" + stringify(qs);
+  }
+  return {
+    "CB-ACCESS-KEY": key,
+    "CB-ACCESS-SIGN": crypto
+      .createHmac("sha256", Buffer.from(secret, "base64"))
+      .update(timestamp + method + uri + (body ? JSON.stringify(body) : ""))
+      .digest("base64"),
+    "CB-ACCESS-TIMESTAMP": timestamp,
+    "CB-ACCESS-PASSPHRASE": passphrase
+  };
+}
