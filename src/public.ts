013332b126ec43788467f8ec2011feeb0be514a6
Sergey Bakulin <vansergen@gmail.com>
2019-11-21 10:35:36 -0500

    feat(public): add getProducts method

--- src/public.ts
+++ src/public.ts
@@ -8,6 +8,25 @@ export const DefaultHeaders = {
   "User-Agent": "coinbase-pro-node-api-client"
 };
 
+export type ProductInfo = {
+  id: string;
+  base_currency: string;
+  quote_currency: string;
+  base_min_size: string;
+  base_max_size: string;
+  base_increment: string;
+  quote_increment: string;
+  display_name: string;
+  min_market_funds: string;
+  max_market_funds: string;
+  margin_enabled: boolean;
+  post_only: boolean;
+  limit_only: boolean;
+  cancel_only: boolean;
+  status: string;
+  status_message: string;
+};
+
 export type PublicClientOptions = {
   product_id?: string;
   sandbox?: boolean;
@@ -28,5 +47,9 @@ export class PublicClient extends RPC {
     const headers = DefaultHeaders;
     super({ useQuerystring, timeout, json: true, baseUrl: apiUri, headers });
     this.product_id = product_id;
+  }
+
+  getProducts(): Promise<ProductInfo[]> {
+    return this.get({ uri: "/products" });
   }
 }
